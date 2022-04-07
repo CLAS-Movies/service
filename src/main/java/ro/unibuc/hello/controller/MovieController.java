248a37706eca692c30272e1d6bedef6769728d45
Claudiu Daramus <38111672+ClaudiuDaramus@users.noreply.github.com>
2022-04-07 12:05:39 +0300

    Monitoring added (#33)

--- src/main/java/ro/unibuc/hello/controller/MovieController.java
+++ src/main/java/ro/unibuc/hello/controller/MovieController.java
@@ -1,8 +1,9 @@
 package ro.unibuc.hello.controller;
 
 import java.util.List;
-
-
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ro.unibuc.hello.dto.MovieDTO;
@@ -14,22 +15,29 @@ public class MovieController {
     @Autowired
     private MovieService movieService;
 
-
+    @Autowired
+    MeterRegistry metricsRegistry;
 
     @GetMapping("/movie/getAll")
     @ResponseBody
+    @Timed(value = "movie.getMovies.time", description = "Time taken to return movies")
+    @Counted(value = "movie.getMovies.count", description = "Times movies were returned")
     public List<MovieDTO> getMovies(){
         return movieService.getMovies();
     }
 
     @GetMapping("/movie/get")
     @ResponseBody
+    @Timed(value = "movie.getMovie.time", description = "Time taken to return a movie")
+    @Counted(value = "movie.getMovie.count", description = "Times a movie was returned")
     public MovieDTO getMovie(@RequestParam(name="id") String id) {
        return movieService.getMovie(id);
     }
 
     @PostMapping("/movie/insert")
     @ResponseBody
+    @Timed(value = "movie.insertMovie.time", description = "Time taken to insert a movie")
+    @Counted(value = "movie.insertMovie.count", description = "Times a movie was inserted")
     public MovieDTO insertMovie(@RequestParam(name="title") String title,
                                 @RequestParam(name="director") String director,
                                 @RequestParam(name="writer") String writer,
@@ -42,6 +50,8 @@ public class MovieController {
 
     @PutMapping("/movie/update")
     @ResponseBody
+    @Timed(value = "movie.updateMovie.time", description = "Time taken to update a movie")
+    @Counted(value = "movie.updateMovie.count", description = "Times a movie was updated")
     public MovieDTO updateMovie(@RequestParam(name="id") String id,
                                 @RequestParam(name="title") String title,
                                 @RequestParam(name="director") String director,
@@ -55,6 +65,8 @@ public class MovieController {
 
     @DeleteMapping("/movie/delete")
     @ResponseBody
+    @Timed(value = "movie.deleteMovie.time", description = "Time taken to delete a movie")
+    @Counted(value = "movie.deleteMovie.count", description = "Times a movie was deleted")
     public String deleteMovie(@RequestParam(name="id") String id) {
         return movieService.deleteMovie(id);
 
--- src/main/java/ro/unibuc/hello/controller/WatchItemController.java
+++ src/main/java/ro/unibuc/hello/controller/WatchItemController.java
@@ -1,5 +1,8 @@
 package ro.unibuc.hello.controller;
 
+import io.micrometer.core.annotation.Counted;
+import io.micrometer.core.annotation.Timed;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import ro.unibuc.hello.dto.WatchItemDTO;
@@ -13,26 +16,37 @@ public class WatchItemController {
     @Autowired
     private WatchItemService watchItemService;
 
+    @Autowired
+    MeterRegistry metricsRegistry;
+
     @GetMapping("/watchItem/getAll")
     @ResponseBody
+    @Timed(value = "watchItem.getWatchItems.time", description = "Time taken to return watchItems")
+    @Counted(value = "watchItem.getWatchItems.count", description = "Times watchItems were returned")
     public List<WatchItemDTO> getWatchItems() {
         return watchItemService.getWatchItems();
     }
 
     @GetMapping("/watchItem/get")
     @ResponseBody
+    @Timed(value = "watchItem.getWatchItem.time", description = "Time taken to return a watchItem")
+    @Counted(value = "watchItem.getWatchItem.count", description = "Times a watchItem was returned")
     public WatchItemDTO getWatchItem(@RequestParam(name="id") String id) {
         return watchItemService.getWatchItem(id);
     }
 
     @PostMapping("/watchItem/insert")
     @ResponseBody
+    @Timed(value = "watchItem.insertWatchItem.time", description = "Time taken to insert a watchItem")
+    @Counted(value = "watchItem.insertWatchItem.count", description = "Times a watchItem was inserted")
     public WatchItemDTO insertWatchItem(@RequestParam(name="movieId") String movieId, @RequestParam(name="userId") String userId) {
         return  watchItemService.insertWatchItem(movieId, userId);
     }
 
     @PutMapping("/watchItem/update")
     @ResponseBody
+    @Timed(value = "watchItem.updateWatchItem.time", description = "Time taken to update a watchItem")
+    @Counted(value = "watchItem.updateWatchItem.count", description = "Times a watchItem was updated")
     public WatchItemDTO updateWatchItem(
             @RequestParam(name="id") String id,
             @RequestParam(name="newMovieId", required = false) String newMovieId, @RequestParam(name="newUserId", required = false) String newUserId
@@ -42,6 +56,8 @@ public class WatchItemController {
 
     @DeleteMapping("/watchItem/delete")
     @ResponseBody
+    @Timed(value = "watchItem.deleteWatchItem.time", description = "Time taken to delete a watchItem")
+    @Counted(value = "watchItem.deleteWatchItem.count", description = "Times a watchItem was deleted")
     public String deleteWatchItem(@RequestParam(name="id") String id) {
         return watchItemService.deleteWatchItem(id);
     }
